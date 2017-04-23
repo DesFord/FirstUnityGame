d846f4a6fe596c0580d1d01b40b9f9a53954040b
Puzzlemaker <bradfordthe3rd@gmail.com>
2017-04-23 16:26:00 -0400

    First pass for grid system

---
+++ Assets/Cell.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UpdateSize();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseUp()
+    {
+        Debug.Log("TEST");
+    }
+
+    void UpdateSize()
+    {
+        Bounds bounds = GetComponent<SpriteRenderer>().bounds;
+        if (bounds.size.x != 1 || bounds.size.y != 1)
+        {
+            Vector3 Scale = new Vector3(1 / bounds.size.x, 1 / bounds.size.y, 1);
+            this.transform.localScale = Scale;
+            //this.GetComponent<BoxCollider2D>().size = new Vector2(1, 1);
+        }
+    }
+}
---
+++ Assets/GameGridBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGridBehavior : MonoBehaviour {
+    public int gridX;
+    public int gridY;
+    public Cell[,] grid;
+    public GameObject cellTemplate;
+
+    // Use this for initialization
+    void Start()
+    {
+        grid = new Cell[gridX, gridY];
+        Random.seed = (int)System.DateTime.Now.Ticks;
+        for (int x = 0; x < gridX; x++)
+        {
+            for (int y = 0; y < gridY; y++)
+            {
+                grid[x, y] = ((GameObject)Instantiate(cellTemplate, new Vector2(x, y), Quaternion.identity)).GetComponent<Cell>();
+            }
+        }
+        //this.GetComponent<BoxCollider2D>().size = new Vector2(gridX, gridY);
+        //this.GetComponent<BoxCollider2D>().offset = new Vector2(gridX / 2, gridY / 2);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
